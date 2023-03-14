a745d99afc7d0c5fd35e2f8ffa2c06edf3e80c7a
Jason Huang <jason97521@gmail.com>
2023-03-14 10:00:11 -0400

    data grid product list

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, Outlet, RouterPro
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
 
 
 function App() {
@@ -17,6 +18,9 @@ function App() {
         <Route path="/users" element={<UserList />} />
         <Route path="/user/:userId" element={<User />} />
         <Route path="/newUser" element={<NewUser />} />
+        <Route path="/products" element={<ProductList />} />
+        {/* <Route path="/products/:productId" element={<ProductList />} />
+        <Route path="/newProducts" element={<ProductList />} /> */}
       </Route>
     )
   )
