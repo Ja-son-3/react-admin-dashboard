747a1736791290a47ad601e2d3b121968267cc1a
Jason Huang <jason97521@gmail.com>
2023-03-14 06:01:11 -0400

    react material ui table - userlist

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css"
 import Home from "./pages/home/Home";
+import { createBrowserRouter, createRoutesFromElements, Route, Outlet, RouterProvider} from 'react-router-dom'
+import UserList from "./pages/userList/UserList";
+
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/users" element={<UserList />} />
+        {/* <Route path="/post/:id" element={<Single />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings/> : <Login />} />
+        <Route path="/register" element={user ? <Home/> : <Register />} /> */}
+      </Route>
+    )
+  )
+
+
   return (
-    <div className="App">
+    <>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <RouterProvider  router={router}/>   
       </div>
-    </div>
+    </>
   );
+}
+
+const Root = () => {
+
+  return (
+    <> 
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
 }
 
 export default App;
